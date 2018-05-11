cfd3f0c49bf1b684f83ac97dddf27c300f5a1fce
jsnlnslt <jani.sulunsilta@student.tut.fi>
2018-05-11 16:11:25 +0300

    method to save measurements from serial done

--- data_handling/DataManager.py
+++ data_handling/DataManager.py
@@ -2,26 +2,74 @@
 import json
 import os
 
-class device:
+# class to represent one measurement
+# values can be added to buffer to prevent continuous
+# disk writing
+class IO:
+    def __init__(self, io):
+        self.io = io
+        self.buffer = []
+    def addToBuffer(
+        self, value):
+        self.buffer.append(value)
+        if len(self.buffer) > 10:
+            #todo
+            #save to file
+            #empty buffer
+            pass
+
+# Class for arduino device, contains deviceId
+# (which is send via rf) and 1+ IO-objects
+class Device:
     def __init__(self, deviceId):
         self.deviceId = deviceId
         self.io = []
     
+    # create IO object and add to list
     def addIO (self, io):
-        self.io.append(io)
+        self.io.append(IO(io))
+    
+    # return IO object
+    def getIO (self):
+        return self.io
 
 
 
 class Devices:
     def __init__(self):
-        pass
+        self.devices = {}
 
     def readSettings(self, filename='C:/Users/janis/Documents/iot/data_handling/Settings.json'):
-        jsonFile = open(filename)
-        data = json.load(jsonFile)
+        try:
+            jsonFile = open(filename)
+            data = json.load(jsonFile)
+        except FileNotFoundError:
+            print("File does not exist")
+        except:
+            print("File reading failed")
 
+        # loop over all device instances and save to devices-dict
         for device in data[0]["devices"]:
-            print(device["deviceId"])
+            d = Device(device["deviceId"])
+            for io in device["IO"]:
+                d.addIO(io)
+            self.devices[d.deviceId] = d
+            print(d.deviceId)
+            print(d.getIO())
+        
+
+        # other settings could be handled here
+
+    def saveFromSerial(self, message):
+        measureList = message.split(" ")
+        IOList = self.devices.get(measureList[0]).getIO()
+        i = 1
+        for io in IOList:
+            # todo: error handling/verification
+            io.addToBuffer(measureList[i])
+
+
+
 
 
 def main():

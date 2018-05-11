bc126761913edfec09c7ecc5db313c65c83306df
Jani Sulunsilta <jani.sulunsilta@student.tut.fi>
2018-05-11 13:35:58 +0300

    Raspberry Pi/Python demo, read serial from arduino and write to HDF5 file

---
+++ data_handling/SerialTest.py
@@ -0,0 +1,26 @@
+import serial
+import pandas as pd
+import datetime
+
+#store = pd.HDFStore('store.h5')
+#store['s'] = pd.Series()#(0, index=datetime.datetime.now().isoformat())
+
+dict = {}
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+s = [0]
+i = 0
+
+
+while True:
+    i = i + 1
+    read_serial = ser.readline()
+    print(read_serial)
+    str = read_serial.split(" ")
+    dict[datetime.datetime.now().isoformat()] = str[0]
+    if i > 10:
+        i = 0
+        timeseries = pd.Series(dict)
+        timeseries.to_hdf('store.h5', 'table', append=True)
+        dict = {}
+	print(pd.read_hdf('store.h5', 'table'))

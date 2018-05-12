48855189556eb00b65f66ac8edab3bedad9b39dc
jsnlnslt <jani.sulunsilta@student.tut.fi>
2018-05-12 14:33:54 +0300

    unit test for readSettings done

--- data_handling/test_DataManager_unitTest.py
+++ data_handling/test_DataManager_unitTest.py
@@ -8,10 +8,16 @@ class TestDataManager(unittest.TestCase):
         self.manager = DataManager.Devices()
     
     def test_readSettings(self):
-        pass
-    
+        self.manager.readSettings('C:/Users/janis/Documents/iot/data_handling/testSettings.json')
+        device1 = self.manager.devices.get("100")
+        self.assertNotEquals(device1, None, "Device 1 not found")
+        IOs = device1.getIO()
+        self.assertEquals(IOs[0].getCode(), "val1")
+        
+
     def test_saveFromSerial(self):
-        pass
+        self.manager.saveFromSerial("888001 10.00 32.00 nan")
+        #result = pd.read_hdf('')
 
 
 if __name__ == '__main__':

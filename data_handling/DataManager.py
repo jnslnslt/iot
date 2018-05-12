2149a7f6b33e1ab84e24fa7769684a772be3352b
jsnlnslt <jani.sulunsilta@student.tut.fi>
2018-05-12 12:33:18 +0300

    Exception handling and verification for saving serial inputs done

--- data_handling/DataManager.py
+++ data_handling/DataManager.py
@@ -2,6 +2,8 @@
 import json
 import os
 
+TARGET_FILE = "store.h5"
+
 # class to represent one measurement
 # values can be added to buffer to prevent continuous
 # disk writing
@@ -9,14 +11,16 @@ class IO:
     def __init__(self, io):
         self.io = io
         self.buffer = []
-    def addToBuffer(
-        self, value):
+    def addToBuffer(self, value):
         self.buffer.append(value)
         if len(self.buffer) > 10:
             #todo
             #save to file
+            #timeSeries = pd.Serial(self.buffer)
+            #timeSeries.to_hdf('store.h5', 'table')
             #empty buffer
-            pass
+            self.buffer = []
+            
 
 # Class for arduino device, contains deviceId
 # (which is send via rf) and 1+ IO-objects
@@ -29,9 +33,12 @@ class Device:
     def addIO (self, io):
         self.io.append(IO(io))
     
-    # return IO object
+    # return IO objects
     def getIO (self):
         return self.io
+    
+    def getIOLength(self):
+        return len(self.io)
 
 
 
@@ -54,22 +61,35 @@ class Devices:
             for io in device["IO"]:
                 d.addIO(io)
             self.devices[d.deviceId] = d
-            print(d.deviceId)
-            print(d.getIO())
-        
 
         # other settings could be handled here
 
+    # function to add values to io buffer
+    # including verification and exception handling
     def saveFromSerial(self, message):
-        measureList = message.split(" ")
-        IOList = self.devices.get(measureList[0]).getIO()
-        i = 1
-        for io in IOList:
-            # todo: error handling/verification
-            io.addToBuffer(measureList[i])
+        values = message.split("")
+        # check if message is empty
+        if (values.empty()):
+            return
+        device = self.devices.get(values[0])
+        # check if device exists in settings
+        if (device == None):
+            return
+        # check if length of message matches device io number
+        elif (len(values)-1 != device.getIOLength()):
+            return
+        else:
+            i = 1 
+            for io in device.getIO():
+                try:
+                    val = float(values[i])
+                except ValueError:
+                    val = 'nan'
+                io.addToBuffer(val)
+          
 
-
-
+            
+        
 
 
 def main():

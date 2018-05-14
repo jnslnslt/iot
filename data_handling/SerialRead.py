c7fcd129441aac7ec8e63d02ad9ba78e7d34b52b
Jani Sulunsilta <jani.sulunsilta@student.tut.fi>
2018-05-14 12:54:29 +0300

    Test print files

--- data_handling/SerialRead.py
+++ data_handling/SerialRead.py
@@ -1,6 +1,6 @@
 import serial
 import DataManager
-
+import pandas as pd
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 manager = DataManager.DataManager()
@@ -11,3 +11,13 @@ while True:
     line = ser.readline()
     print(line)
     manager.saveFromSerial(line)
+    i +=1
+    if i >= 100:
+	val = pd.read_hdf('Arduino1 - tempMeasure.h5', 'T01')
+	print(val)
+	val = pd.read_hdf('Arduino1 - tempMeasure.h5', 'T02')
+	print(val)
+	val = pd.read_hdf('Arduino1 - tempMeasure.h5', 'H01')
+	print(val)
+	i = 0
+	
